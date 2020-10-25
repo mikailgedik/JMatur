676e4495545b9ead94e6b102e53d94fd37990d1a
Mikail Gedik <mikail.gedik@bluewin.ch>
2020-10-25 13:26:08 +0100

    GPU image creation TODO: free memory, memory management, don't create unnecessary clusters

--- mikailgedik.jmatur.backend/src/ch/mikailgedik/kzn/matur/backend/render/ImageCreator.java
+++ mikailgedik.jmatur.backend/src/ch/mikailgedik/kzn/matur/backend/render/ImageCreator.java
@@ -1,77 +1,9 @@
 package ch.mikailgedik.kzn.matur.backend.render;
 
 import ch.mikailgedik.kzn.matur.backend.connector.Screen;
-import ch.mikailgedik.kzn.matur.backend.data.*;
-import ch.mikailgedik.kzn.matur.backend.data.value.Value;
+import ch.mikailgedik.kzn.matur.backend.data.Region;
 
-import java.util.ArrayList;
-
-public class ImageCreator<T extends Value> {
-    private ColorFunction<T> colorFunction;
-    private DataSet<T> dataSet;
-    private ImageResult<T> imageResult;
-    private ArrayList<ImageResult<T>> buffer;
-
-    public ImageCreator(DataSet<T> dataSet, ColorFunction<T> colorFunction) {
-        this.colorFunction = colorFunction;
-        this.dataSet = dataSet;
-        buffer = new ArrayList<>();
-    }
-
-    /** Does not scale the images down to minPixelWidth and minPixelHeight, but guarantees that the returned Screen has always bigger dimensions*/
-    public Screen createScreen(int minPixelWidth, int minPixelHeight, Region region, int threads, long maxWaitingTime) {
-        //TODO buffer image results to avoid creating same images over and over
-        //Only cropping has to be done anew
-
-        imageResult = new ImageResult<>(minPixelWidth, minPixelHeight, region, colorFunction, dataSet);
-        boolean create = true;
-        LogicalRegion tr;
-        for(ImageResult<T> i: buffer) {
-            tr = i.getLogicalRegion();
-            if(tr.getStartX() <= imageResult.getLogicalRegion().getStartX() &&
-                    tr.getStartY() <= imageResult.getLogicalRegion().getStartY() &&
-                    tr.getEndX() >= imageResult.getLogicalRegion().getEndX() &&
-                    tr.getEndY() >= imageResult.getLogicalRegion().getEndY()) {
-                create = false;
-                imageResult = i;
-                break;
-            }
-        }
-
-        if(create) {
-            imageResult.create(threads, maxWaitingTime);
-            buffer.add(imageResult);
-        }
-
-        return getCutVersion(region);
-    }
-
-    private Screen getCutVersion(Region region) {
-        Screen result = imageResult.getResult();
-        Region actualRegion = imageResult.getActualRegion();
-
-        Screen cutVersion;
-
-        double sx = ((region.getStartX() - actualRegion.getStartX()) / actualRegion.getWidth() * result.getWidth()),
-                sy = ((region.getStartY() - actualRegion.getStartY()) / actualRegion.getHeight() * result.getHeight()),
-                ex = ((region.getEndX() - actualRegion.getStartX()) / actualRegion.getWidth() * result.getWidth()) -1,
-                ey = ((region.getEndY() - actualRegion.getStartY()) / actualRegion.getHeight() * result.getHeight())-1;
-
-        //Assert no upscaling...
-        assert sx >= 0;
-        assert sy >= 0;
-        assert ex < result.getWidth();
-        assert ey < result.getHeight();
-
-        cutVersion = result.subScreen((int)sx, (int)sy,(int)(ex -sx), (int)(ey -sy));
-
-        int[] row = new int[cutVersion.getWidth()];
-        for(int y = 0; y < cutVersion.getHeight()/2; y++) {
-            System.arraycopy(cutVersion.getPixels(), y * cutVersion.getWidth(), row, 0, cutVersion.getWidth());
-            System.arraycopy(cutVersion.getPixels(), (cutVersion.getHeight() - 1 - y) * cutVersion.getWidth(), cutVersion.getPixels(),y * cutVersion.getWidth(), cutVersion.getWidth());
-            System.arraycopy(row, 0, cutVersion.getPixels(),(cutVersion.getHeight() - 1 - y) * cutVersion.getWidth(), cutVersion.getWidth());
-        }
-
-        return cutVersion;
-    }
+public abstract class ImageCreator {
+    public abstract Screen createScreen(int minPixelWidth, int minPixelHeight, Region region,
+                               int threads, long maxWaitingTime);
 }
